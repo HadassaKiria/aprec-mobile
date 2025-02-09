b0c20b5f9d16c3f149cccba07bdb1c6e89f59f35
gabriel alencar <gabielgomes2001@gmail.com>
2025-02-08 22:29:29 -0300

    feat: components : inputProfile

---
+++ src/components/input-profile/styles.ts
@@ -0,0 +1,20 @@
+import { StyleSheet } from "react-native";
+
+import { colors, fontFamily } from "../../styles/theme"
+
+export const s = StyleSheet.create({
+    container:{
+        width: '100%',
+        paddingVertical: 34,
+        paddingHorizontal: 8
+    },
+    forgotPassword: {
+        fontSize: 15,
+        fontFamily: fontFamily.regular,
+        textAlign: "right",
+        marginRight: "4%",
+        marginBottom: 20,
+        color: colors.blue[100],
+        textDecorationLine: 'underline'
+    }
+})
