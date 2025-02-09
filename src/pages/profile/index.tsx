af79992cea415677c96a0ae0dd04e458fc4a8282
gabriel alencar <gabielgomes2001@gmail.com>
2025-02-08 22:32:43 -0300

    feat: page-inicio de Profiles.

---
+++ src/pages/profile/index.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { View, Image } from 'react-native';
+
+import { s } from "./styles"
+
+import { Button } from "../../components/button"
+import { InputLogin } from '../../components/input-login.tsx';
+import { InputProfile } from '../../components/input-profile';
+
+export default function SignIn(){
+    return(
+        <View style={s.container}>
+            <Image
+                source={require('../../assets/logo.png')}
+            />
+
+            <InputProfile></InputProfile>
+            <Button
+                title="Atualizar dados" 
+                onPress={() => ""}>
+            </Button>
+            <Button 
+                title="Alterar senha" 
+                onPress={() => ""}
+                variant="outline">
+            </Button>
+
+        </View>
+    )
+}
